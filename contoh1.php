ff13bb96cf06eb898255847cb6d30e049c22e577
randihack69 <90668194+randihack69@users.noreply.github.com>
2021-09-15 21:20:09 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+	{
+		echo "<h1>perkenalkan</h1>";
+		echo "Nama saya randi
+		    saya tinggal di kapuas hulu
+			olahraga yang saya sukai adalah
+			futsal";
+    }
+}
